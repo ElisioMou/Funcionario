4f60f10c425ba7d0210910d7fbd919d641a9f256
Élísio Moura <elisiomou@gmail.com>
2020-06-25 15:25:38 -0300

    Estes arquivos estão prontos para commitar.

---
+++ ClassesExercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesExercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine();
+            Console.Write("Qual porcentagem deseja aumentar o salário? " );
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+            Console.WriteLine();
+        }
+    }
+}
